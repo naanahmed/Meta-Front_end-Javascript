c85a8b0248d4d3894c5bdb1551778a1cc344b42a
Naan Ahmed <naanahmed.na@gmail.com>
2023-01-16 22:21:45 +0300

    Add for of loops.js

---
+++ Week 3/Advanced Javascript Features/for of loops.js
@@ -0,0 +1,15 @@
+/*
+const car ={
+    speed : 100,
+    color : "blue"
+}
+
+for (prop of car){
+    console.log(car)
+}
+*/
+
+const cars =[1200,'benzene', 'blue']
+for (var car of cars){
+    console.log(car);
+}

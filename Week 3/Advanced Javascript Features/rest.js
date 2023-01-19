d1ddba632d0c79ba16257d5fb237e234a8caa93d
naanahmed <naanahmed.na@gmail.com>
2023-01-19 18:49:38 +0300

    rest.js

---
+++ Week 3/Advanced Javascript Features/rest.js
@@ -0,0 +1,10 @@
+const top_7=['IceCream', 'Fries', 'Chocolate', 'Cake', 'Soda', 'MilkShake', 'Lebenya']
+
+const [] = top_7
+const[first, second, third, ...secondBatch] = top_7
+
+console.log(first)
+console.log(second)
+console.log(third)
+console.log(secondBatch)
+

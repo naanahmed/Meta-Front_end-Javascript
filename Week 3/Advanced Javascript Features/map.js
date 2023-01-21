00a1786e49a8822d2b458512fccce74cec3be5bc
Naan Mohammed <naanahmed24@gmail.com>
2023-01-21 16:28:36 +0300

    Add map.js

---
+++ Week 3/Advanced Javascript Features/map.js
@@ -0,0 +1,11 @@
+/*
+[0,10,20,30,40,50].map( function(num) {
+    return num / 10
+})
+*/
+
+[1000,2000,2500,3000,3500,4000].map( function(dollars){
+    return dollars * 100
+})
+
+//why dont two .maps work in the same page
